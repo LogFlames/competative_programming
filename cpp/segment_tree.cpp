e11e10a206c630545e8735ca3e3dfec27ccd7c0f
Elias Lundell <elilun03@gmail.com>
2021-11-24 08:13:52 +0100

    More general segment tree

--- cpp/segment_tree.cpp
+++ cpp/segment_tree.cpp
@@ -1,24 +1,34 @@
 #include <vector>
 
 struct Tree {
+    int (*fnPtr)(int, int);
     std::vector<int> s;
     int n;
-    Tree(int n = 0) : s(2 * n, 0), n(n) {}
+    Tree(int n, int (*fnPtr)(int, int)) : fnPtr(fnPtr), s(2 * n, 0), n(n) {}
+    Tree(int n, std::vector<int> A, int (*fnPtr)(int, int))
+        : fnPtr(fnPtr), s(2 * n), n(n) {
+        for(int i = 0; i < A.size(); i++) {
+            s[i + n] = A[i];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            s[i] = (*fnPtr)(s[i * 2], s[i * 2 + i]);
+        }
+    }
     void update(int i, int v) {
         int ind = i + n;
         s[ind] = v;
         while (ind != 0) {
             ind /= 2;
-            s[ind] = s[ind * 2] + s[ind * 2 + 1];
+            s[ind] = (*fnPtr)(s[ind * 2], s[ind * 2 + 1]);
         }
     }
     int query(int l, int r) {
         int sl = 0, sr = 0;
         for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
-            if (l % 2 == 1) sl += s[l++];
-            if (r % 2 == 1) sr += s[--r];
+            if (l % 2 == 1) sl = (*fnPtr)(sl, s[l++]);
+            if (r % 2 == 1) sr = (*fnPtr)(sr, s[--r]);
         }
-        return sl + sr;
+        return (*fnPtr)(sl, sr);
     }
 };
 
