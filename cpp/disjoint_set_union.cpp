b0ae68c0a411645b8fd5c9b7a62f567eee03243d
Elias Lundell <elilun03@gmail.com>
2022-01-23 15:44:25 +0100

    Link to larger to avoid long chains of links

--- cpp/disjoint_set_union.cpp
+++ cpp/disjoint_set_union.cpp
@@ -13,6 +13,10 @@ void union_sets(int a, int b, vi& parent) {
     a = find_set(a, parent);
     b = find_set(b, parent);
     if (a != b) {
-        parent[b] = a;
+        if (a > b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
     }
 }
