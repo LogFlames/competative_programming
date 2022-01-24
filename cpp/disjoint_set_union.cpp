61c662eb11b4741d53624b7f8a329bd5b0496bc9
Elias Lundell <elilun03@gmail.com>
2022-01-24 11:22:52 +0100

    Write out vector

--- cpp/disjoint_set_union.cpp
+++ cpp/disjoint_set_union.cpp
@@ -9,7 +9,7 @@ int find_set(int v, std::vector<int>& parent) {
     return p;
 }
 
-void union_sets(int a, int b, vi& parent) {
+void union_sets(int a, int b, std::vector<int>& parent) {
     a = find_set(a, parent);
     b = find_set(b, parent);
     if (a != b) {
